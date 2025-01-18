4698240bd91f7881e64bc7ab4e2595dc9358c25d
bakdaul3t <yzentor@gmail.com>
2025-01-18 23:45:12 +0500

    ad

---
+++ data_types/2.py
@@ -0,0 +1,15 @@
+a = "Hello World"
+b = 20
+c = 20.5
+d = 1j
+e = ["apple", "banana", "cherry"]
+f = ("apple", "banana", "cherry")
+g = range(6)
+h = {"name" : "John", "age" : 36}
+j = {"apple", "banana", "cherry"}
+k = frozenset({"apple", "banana", "cherry"})
+l = True
+t = b"Hello"
+y = bytearray(5)
+u = memoryview(bytes(5))
+i = None
